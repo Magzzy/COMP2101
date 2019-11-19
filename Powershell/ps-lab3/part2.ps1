568580811e75bdd8acd527db95f6e21673bf6f23
Magzzy <43187688+Magzzy@users.noreply.github.com>
2019-11-18 23:22:28 -0500

    Create part2.ps1

---
+++ Powershell/ps-lab3/part2.ps1
@@ -0,0 +1,9 @@
+﻿get-wmiobject -class win32_product -filter "vendor!='Microsoft Corporation'" | sort InstallDate | format-table -autosize @{n="Installed On";
+    e={([datetime]($_.installdate.substring(0,4) + " " + 
+    $_.installdate.substring(4,2) + " " + 
+    $_.installdate.substring(6,2))).ToString("ddd MMM,dd,yyyy")}}, Vendor, Name, Version
+
+get-wmiobject -class win32_product | ? Vendor -NotMatch "Microsoft Corporation" | sort InstallDate | format-table -autosize @{n="Installed On";
+    e={([datetime]($_.installdate.substring(0,4) + " " + 
+    $_.installdate.substring(4,2) + " " + 
+    $_.installdate.substring(6,2))).ToString("ddd MMM,dd,yyyy")}}, Vendor, Name, Version
