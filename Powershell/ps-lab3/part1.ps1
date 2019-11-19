0fb66c6efbfec5d876109b34e0f86fdc53a224ab
Magzzy <43187688+Magzzy@users.noreply.github.com>
2019-11-19 13:18:16 -0500

    Create part1.ps1

---
+++ Powershell/ps-lab3/part1.ps1
@@ -0,0 +1,3 @@
+﻿get-wmiobject -class win32_logicaldisk | ? Size -GT "0" | format-table -AutoSize DeviceID, @{n="Size     ";
+    e={[math]::Round($_.Size/1GB,2).ToString()+"GB"}}, 
+    @{n="Free Space";e={[math]::Round($_.FreeSpace/1GB,2).ToString()+"GB"}}, ProviderName
